7c71887ecd77a00c0983f56272b1d55575453a18
Ubuntu <ubuntu@ip-172-31-0-158.ec2.internal>
2026-06-28 13:35:28 +0000

    Updated Process_finder.py to select only processes which have main.py written as part of its command to generate it

--- Process_Finder.py
+++ Process_Finder.py
@@ -9,12 +9,16 @@ class Process_Finder:
     def __init__(self):
         pass
     def get_process_list(self):
-        pid_list: str = subprocess.run(["ps", "-lax", "|", "grep", "main.py"], capture_output=True, text=True)
-        filtered_pid_list = filter_process_list(pid_list)
+        pid_list: str = subprocess.run(["ps", "-lax"], capture_output=True, text=True)
+        back_to_lines = pid_list.stdout.splitlines()
+        #for line in back_to_lines:
+        #    print(line)
+
+        filtered_pid_list = self.filter_process_list(back_to_lines)
 
         print(filtered_pid_list)
-    def filter_process_list(self, unfiltered_list: str) -> str:
-        string_list: list
+    def filter_process_list(self, unfiltered_list: list) -> list:
+        string_list: list = []
         process_identifier: str = "main.py"
         for line in unfiltered_list:
             if process_identifier in line:
