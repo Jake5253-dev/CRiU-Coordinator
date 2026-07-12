edfeb248c85496564919645e3666cb3375475547
Jake5253-dev <jakesirr@hotmail>
2026-07-12 17:40:30 +0100

    removed positional argument in self.__return_criu_dump_folder method

--- CriuDumper.py
+++ CriuDumper.py
@@ -22,7 +22,7 @@ class CriuDumper:
 
         PID = PID_num 
         result = subprocess.run(["sudo", "criu", "dump", "-t", str(PID)
-        ,"-D", str(self.__return_criu_dump_folder(self)), "-vvv"])
+        ,"-D", str(self.__return_criu_dump_folder()), "-vvv"])
         if result.returncode == 0:
             print("OK")
         else:
