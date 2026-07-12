4fdea52a489f594f66605a12776f3139954099d4
Jake5253-dev <jakesirr@hotmail>
2026-07-12 14:34:33 +0100

    removed line break, not required, updated else: to elif

--- CriuDumper.py
+++ CriuDumper.py
@@ -20,11 +20,11 @@ class CriuDumper:
         #TODO create a more sophisticated dump file storage system
 
         PID = PID_num 
-        result = subprocess.run(["sudo", "criu", "dump", "-t", str(PID)\ 
+        result = subprocess.run(["sudo", "criu", "dump", "-t", str(PID)
         ,"-D", str(__return_criu_dump_folder(self)), "-vvv"])
         if result.returncode == 0:
             print("OK")
-        else:
+        elif:
             print("Dumping Failed")
     
     def __return_criu_dump_folder(self):
