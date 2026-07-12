031c569fbecca6e70e03441dbfcbc201a6ae67b3
Jake5253-dev <jakesirr@hotmail>
2026-07-12 16:46:17 +0100

    changed method of splitting process string into a list of each component of that string

--- CriuRunner.py
+++ CriuRunner.py
@@ -50,8 +50,8 @@ class CriuRunner:
             count = 0
             for process in self.active_daemon_PID:
                 count = count + 1
-                back_to_lines = process.stdout.splitlines()
-                process_dict[count] = back_to_lines[2]
+                process_string_list = process.split()
+                process_dict[count] = process_string_list[2]
             for key, value in process_dict.items():
                 print(f"{key}: {value}")
         # self.cd.dump_daemon_process()
