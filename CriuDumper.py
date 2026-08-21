8fe9fba85ce479a0587eec1d44edaa8f41c0b8e7
Jake5253-dev <jakesirr@hotmail>
2026-08-21 14:50:17 +0100

    Added additional type hints with help of mypy, added TODO

--- CriuDumper.py
+++ CriuDumper.py
@@ -8,7 +8,7 @@ class CriuDumper:
 
     def __init__(self):
 
-        self.__dump_folder_path_string = os.getcwd()+ "/CriuDumps"
+        self.__dump_folder_path_string: str = os.getcwd()+ "/CriuDumps"
         if not  os.path.exists(self.__dump_folder_path_string):
             os.mkdir(self.__dump_folder_path_string)
 
@@ -19,23 +19,24 @@ class CriuDumper:
         and process is started as a new session, not connected to a terminal
         Moves the dump into a local sub folder called CriuDumps, and generates a folder to store the contents of the dump """
 
-        folder_name = self.__create_folder_for_PID(PID_num)
+        folder_name: str = self.__create_folder_for_PID(PID_num)
+        #TODO: remove dump log generation and associated methods
         self.__dump_logging_new_entry()
-        result = subprocess.run(["sudo", "criu", "dump", "-t", str(PID_num)
+        result: subprocess.CompletedProcess = subprocess.run(["sudo", "criu", "dump", "-t", str(PID_num)
         ,"-D", str(folder_name), "-vvv", "--log-file", "dump_details.txt"])
         if result.returncode == 0:
             print("OK")
         else:
             print("Dumping Failed")
     
-    def __return_criu_dump_folder(self):
+    def __return_criu_dump_folder(self) -> str:
         return self.__dump_folder_path_string
 
     def __dump_logging_new_entry(self):
         with open("dump_log.txt","a") as f:
             f.write(self.__new_dump_log_paragraph())
 
-    def __new_dump_log_paragraph(self):
+    def __new_dump_log_paragraph(self) -> str:
         formatting_string = "-------------------\n New Entry\n -------------------\n"
         return formatting_string
 
@@ -45,7 +46,7 @@ class CriuDumper:
 
         now = datetime.now()
         # format day, three letter month, year, hrs, mins, secs, milisecs
-        new_folder_string = "PID_" + PID_num + "__" + now.strftime("%d-%b-%Y__%Hhrs-%Mmin-%Ssec-%fms")
-        return_string = self.__dump_folder_path_string + "/" + new_folder_string
+        new_folder_string: str = "PID_" + PID_num + "__" + now.strftime("%d-%b-%Y__%Hhrs-%Mmin-%Ssec-%fms")
+        return_string: str = self.__dump_folder_path_string + "/" + new_folder_string
         os.mkdir(return_string)
         return return_string
